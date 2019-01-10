5876885f6e0aae4888cc0eac8c10f7a3d870e3c0
Luis A. Peña Zuñiga <lapena@grupomavi.com>
2019-01-10 12:22:08 -0600

    Extrae objetos y variables del objeto

---
+++ LeerCopiarArchivo.js
@@ -0,0 +1,30 @@
+
+const fs = require('fs')
+const iconvlite = require('iconv-lite')
+
+function readDemo1(file1) {
+    return  recodificar(file1, 'ascii')
+}
+
+function recodificar(archivo, recodificacion) {
+    return iconvlite.decode(fs.readFileSync(archivo), recodificacion)
+}
+
+function writeDemo2(carpetaDestino, archivo, dataDemo1) {
+    fs.writeFileSync(carpetaDestino + archivo.replace(/.*\\/g, ''), dataDemo1, 'ascii')
+    console.log('archivo creado')
+  }
+
+exports.copyFile = function (carpetaDestino, archivo) {
+
+    try {
+        let dataDemo1 = readDemo1(archivo)
+        dataDemo1 += '\n' +  readDemo1(archivo)
+
+        writeDemo2(carpetaDestino, archivo, dataDemo1)
+        //console.log(dataDemo1)
+    } catch (error) {
+        console.error(error);
+    }
+}
+
