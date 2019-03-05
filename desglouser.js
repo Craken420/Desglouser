f8db4270fca87042659ee55435e643f99683869e
Luis Angel <luis.pena.15s@utzmg.edu.mx>
2019-03-05 13:44:52 -0600

    Se agrego el reporte de relacion de objetos

--- desglouser.js
+++ desglouser.js
@@ -21,30 +21,30 @@ let sendObj = JSON.parse( JSON.stringify( objFile ) )
 
 obj[rgx.Borrar.clsRuta(pathFile)] = desglozar(continua(sendObj))
 
-console.log(obj)
+// console.log(obj)
 
 //Recorrer el arreglo de formas y por cada una buscar si existe y volver a ejecutar
 
 /*** Recorrer el detalle ***/
 
-// for (path in obj) {
-//     //console.log(obj[key])
-//     for (cmp in obj[path]['Detalle']) {
-//         //console.log(obj[path]['Detalle'][cmp])
-//         console.log('[' + cmp + ']')
-//         for (field in obj[path]['Detalle'][cmp]) {
-//             // console.log(obj[path]['Detalle'][cmp][field])
-//             if (obj[path]['Detalle'][cmp][field]['objIntls']) {
-//                 console.log(field + '= ',
-//                 obj[path]['Detalle'][cmp][field]['objIntls'])
-//                 // console.log('-----------------------------------------------')
-//             }
-//             if (obj[path]['Detalle'][cmp][field]['objSQL']) {
-//                 // console.log('-----------------------------------------------')
-//                 console.log(field + '= ',
-//                 obj[path]['Detalle'][cmp][field]['objSQL'])
-//                 // console.log('-----------------------------------------------')
-//             }
-//         }
-//     }
-// }
+for (path in obj) {
+    // console.log(obj[path])
+    for (cmp in obj[path]['Detalle']) {
+        // console.log(obj[path]['Detalle'][cmp])
+        // console.log('[' + cmp + ']')
+        for (field in obj[path]['Detalle'][cmp]) {
+            // console.log(obj[path]['Detalle'][cmp][field])
+            if (obj[path]['Detalle'][cmp][field]['objIntls']) {
+                // console.log(field + '= ',
+                obj[path]['Detalle'][cmp][field]['objIntls']
+                // console.log('-----------------------------------------------')
+            }
+            if (obj[path]['Detalle'][cmp][field]['objSQL']) {
+                // console.log('-----------------------------------------------')
+                // console.log(field + '= ',
+                obj[path]['Detalle'][cmp][field]['objSQL']
+                // console.log('-----------------------------------------------')
+            }
+        }
+    }
+}
