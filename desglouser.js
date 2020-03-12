4128f5db8b7e914e5bc9cd90f41590e6386b1cff
Craken420 <43248911+Craken420@users.noreply.github.com>
2020-03-12 17:18:09 -0600

    Update desglouser.js

--- desglouser.js
+++ desglouser.js
@@ -2,12 +2,15 @@ const { generarObj } = require('./Utilerias/OperadorObjetos/desglozar')
 const { leerCarpetaFiltrada } = require('./Utilerias/OperadoresArchivos/readDirOnlyFile')
 
 const carpeta = 'Testing\\'
-const file = 'C:\\Users\\lapena\\Documents\\Luis Angel\\Sección Mavi\\Intelisis\\Intelisis5000\\Reportes MAVI\\FormaValor.vis'
-const carpetaReportes = 'C:\\Users\\lapena\\Documents\\Luis Angel\\Sección Mavi\\Intelisis\\Intelisis5000\\Reportes MAVI'
+const file = 'Testing\\FormaValor.vis'
 
-// generarObj(file)
+/* Usage */ 
 
-leerCarpetaFiltrada(carpetaReportes, ['.frm', '.tbl', '.vis', '.dlg', '.rep'])
+// Path File
+generarObj(file)
+
+// Path dir
+leerCarpetaFiltrada(carpeta, ['.frm', '.tbl', '.vis', '.dlg', '.rep'])
 .then(archivos => {
     
     archivos.forEach(x => {
